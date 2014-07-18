a4b1a9cf3b20bf717407a1a3e358f111f4b32ee7
Kevin Lee <klee@serena.com>
2014-07-18 16:38:44 +0100

    Migrated to simple User model

---
+++ grails-app/controllers/com/serena/radar/UserController.groovy
@@ -0,0 +1,95 @@
+package com.serena.radar
+
+import grails.plugins.rest.client.RestBuilder
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class UserController {
+    def scaffold = User
+
+    def login() {}
+
+    def authenticate() {
+        session.sraUrl = params.url
+        if (validateAutomationConnection(params.url, params.username, params.password, params.rememberMe)) {
+            // if user doesn't exist
+            def user = User.findByLogin(params.username)
+            if (user == null) {
+                // create them
+                user = new User(login: params.username, name: params.username, password: params.password)
+                user.save()
+            }
+            session.user = user
+
+            // if user settings doesn't exist
+            def settings = Settings.findByUsername(params.username)
+            if (settings == null) {
+                // create them
+                settings = new Settings(username: params.username, sraUrl: params.url, refreshInterval: 10)
+                settings.save()
+            }
+            session.sraUrl = settings.sraUrl
+            session.refreshInterval = settings.refreshInterval
+
+            flash.message = message(code: 'login.success', args: [params.username])
+            redirect(controller: "dashboard", action: "view")
+        } else {
+            flash.message = message(code: 'login.authentication.error', args: [params.username])
+            redirect(action: "login")
+        }
+    }
+
+    def logout() {
+        flash.message = message(code: 'logout.success', args: [session.user.name])
+        session.user = null
+        redirect(controller: "dashboard", action: "view")
+    }
+
+    def boolean validateAutomationConnection(final String url, final String username,
+                                             final String password, final String rememberMe) {
+        try {
+            RestBuilder rest = new RestBuilder()
+            def resp = rest.get(url + "/rest/state") {
+                auth(username, password)
+                header 'DirectSsoInteraction', 'true'
+                accept("application/json")
+                contentType("application/json")
+            }
+            return (resp.status >= 200 && resp.status < 300)
+        } catch (Exception e) {
+            return false
+        }
+    }
+
+    // TODO: replace validateAutomationConnection with below...
+    def boolean validAutomationUser(final String url, final String username, final String password,
+                                       final String rememberMe) {
+        boolean isSuccess = true
+        def setupErrorMsg = message(code: "login.server.not.setup")
+        def authErrorMsg  = message(code: "login.server.not.authenticated")
+
+        RestBuilder rest = new RestBuilder()
+        MultiValueMap<String, String> form = new LinkedMultiValueMap<String, String>()
+        form.add("username", username)
+        form.add("password", password)
+        form.add("rememberMe", "false")
+        form.add("Log In", "login")
+        form.add("requestedHash", "")
+
+        def resp = rest.post("http://localhost:8080/serena_ra/tasks/LoginTasks/login") {
+            contentType("application/x-www-form-urlencoded")
+            body(form)
+        }
+
+        println resp.status
+        println resp.text
+
+        if (resp.text.contains(setupErrorMsg) || resp.text.contains(authErrorMsg) || resp.text.isEmpty())
+            isSuccess = false
+        return ((resp.status >= 200 && resp.status < 300) && isSuccess)
+
+    }
+
+}
---
+++ grails-app/domain/com/serena/radar/User.groovy
@@ -0,0 +1,20 @@
+package com.serena.radar
+
+class User {
+
+    static constraints = {
+        login(unique:true)
+        password()
+        name()
+        email()
+    }
+
+    String login
+    String password
+    String name
+    String email
+
+    String toString() {
+        name + " (" + login +")"
+    }
+}
